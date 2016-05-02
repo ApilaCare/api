21c45e5dff79c3a581cb623f43ba2aadbf696bb0
KyleRiggen <kyle.riggenbach@gmail.com>
2016-05-01 21:37:43 -0600

    api clean up

--- apila.js
+++ apila.js
@@ -1,13 +1,9 @@
 require('dotenv').load();
 var debug = require('debug')('Express4');
 var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var uglifyJs = require("uglify-js");
-var fs = require('fs');
 var passport = require('passport');
 
 require('./models/db');
--- routes/index.js
+++ routes/index.js
@@ -8,28 +8,12 @@ var auth = jwt({
     userProperty: 'payload'
 });
 
-var ctrlLocations = require('../controllers/locations');
 var ctrlIssues = require('../controllers/issues');
 var ctrlResidents = require('../controllers/residents');
 var ctrlAppointments = require('../controllers/appointments');
-var ctrlReviews = require('../controllers/reviews');
 var ctrlIssueComments = require('../controllers/issueComments');
 var ctrlAppointmentComments = require('../controllers/appointmentComments');
 var ctrlAuth = require('../controllers/authentication');
-
-// locations
-router.get('/locations', ctrlLocations.locationsListByDistance);
-router.post('/locations', ctrlLocations.locationsCreate);
-router.get('/locations/:locationid', ctrlLocations.locationsReadOne);
-router.put('/locations/:locationid', ctrlLocations.locationsUpdateOne);
-router.delete('/locations/:locationid', ctrlLocations.locationsDeleteOne);
-
-// reviews
-// only logged in users can post, put, or delete reviews
-router.post('/locations/:locationid/reviews', auth, ctrlReviews.reviewsCreate);
-router.get('/locations/:locationid/reviews/:reviewid', ctrlReviews.reviewsReadOne);
-router.put('/locations/:locationid/reviews/:reviewid', auth, ctrlReviews.reviewsUpdateOne);
-router.delete('/locations/:locationid/reviews/:reviewid', auth, ctrlReviews.reviewsDeleteOne);
 
 // authentication
 router.post('/register', ctrlAuth.register);
