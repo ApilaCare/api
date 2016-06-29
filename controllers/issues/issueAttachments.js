68b1d885f05ee169cbbbd74734d90ad3d9dd5acd
nenad <nesa993@gmail.com>
2016-06-29 18:33:40 +0200

    uploading files to amazon aws and storing the links

--- controllers/issues/issueAttachments.js
+++ controllers/issues/issueAttachments.js
@@ -2,6 +2,21 @@ var mongoose = require('mongoose');
 var Iss = mongoose.model('Issue');
 var User = mongoose.model('User');
 
+var fs = require('fs');
+
+var AWS = require('aws-sdk');
+
+AWS.config.update(
+ {
+  accessKeyId: process.env.ACCESS_KEY_ID,
+  secretAccessKey: process.env.SECRET_ACCESS_KEY
+});
+
+var region = "s3-us-west-2";
+var bucket = "apilatest2";
+
+var s3bucket = new AWS.S3({params: {Bucket: bucket}});
+
 var sendJSONresponse = function(res, status, content) {
     res.status(status);
     res.json(content);
@@ -74,39 +89,50 @@ var doAddAttachment = function(req, res, issue, username) {
 
     var file = req.files.file;
 
-    if (!issue) {
-        sendJSONresponse(res, 404, "issueid not found");
-    } else {
+    var stream = fs.createReadStream(file.path);
 
-      var apiUrl = "http://localhost:3300/files/";
+    var params = {Key: file.originalFilename, Body: stream};
+     s3bucket.upload(params, function(err, data) {
+       fs.unlinkSync(file.path);
+       if (err) {
+         console.log("Error uploading data: ", err);
+       } else {
+         console.log("Successfully uploaded data aws");
 
-      if (process.env.NODE_ENV === 'production') {
-        apiUrl = process.env.API_URL;
-      }
+         if (!issue) {
+             sendJSONresponse(res, 404, "issueid not found");
+         } else {
 
-      var fullUrl = apiUrl + file.path;
-    //   var fullUrl = file.path;
-        issue.attachments.push({
-            uploader: req.payload.name,
-            name: file.originalFilename,
-            source: file.path,
-            url: fullUrl,
-            type: file.type,
-        });
 
-        console.log(issue);
-        issue.updateInfo.push(req.body.updateInfo);
+           var fullUrl = "https://" + region + ".amazonaws.com/" + bucket + "/"
+                                                + escape(file.originalFilename);
 
-        issue.save(function(err, issue) {
-            var thisAttachment;
-            if (err) {
-                sendJSONresponse(res, 400, err);
-            } else {
-                thisAttachment = issue.attachments[issue.attachments.length - 1];
-                sendJSONresponse(res, 201, thisAttachment);
-            }
-        });
-    }
+           console.log(fullUrl);
+
+             issue.attachments.push({
+                 uploader: req.payload.name,
+                 name: file.originalFilename,
+                 source: file.path,
+                 url: fullUrl,
+                 type: file.type,
+             });
+
+             issue.updateInfo.push(req.body.updateInfo);
+
+             issue.save(function(err, issue) {
+                 var thisAttachment;
+                 if (err) {
+                     sendJSONresponse(res, 400, err);
+                 } else {
+                     thisAttachment = issue.attachments[issue.attachments.length - 1];
+                     sendJSONresponse(res, 201, thisAttachment);
+                 }
+             });
+         }
+       }
+     });
+
+
 };
 
 
