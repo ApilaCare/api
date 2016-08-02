ff124d04c4f6b002c3fd05e56541b3606043b2cb
nenad <nesa993@gmail.com>
2016-08-02 14:55:01 +0200

    adding seed.js file by hand

---
+++ seed.js
@@ -0,0 +1,64 @@
+var mongoose = require('mongoose');
+var async = require('async');
+
+require('./models/db');
+
+var User = mongoose.model('User');
+var Community = mongoose.model('Community');
+
+var USERS_NUM = 5;
+
+var usersId = [];
+var communityId = [];
+
+mongoose.connection.on('connected', function() {
+
+      mongoose.connection.db.dropDatabase();
+
+      for(var i = 0; i < USERS_NUM; ++i) {
+        createUser("test" + i, "test" + i +"@gmail.com", function(userid) {
+          createTestCommunity("test", true, userid);
+        });
+      }
+
+});
+
+// create dummy user
+function createUser(name, email, callback) {
+  var user = new User();
+
+  user.name = name;
+  user.email = email;
+
+  // use setPassword method to set salt and hash
+  user.setPassword("123456");
+
+  user.save(function(err, user) {
+    console.log(user);
+    usersId.push(user._id);
+    callback(user._id);
+  });
+};
+
+function createTestCommunity(name, test, creator) {
+  Community.create({
+      "name" : name,
+      "communityMembers" : [creator],
+      "pendingMembers" : [],
+      "directors" : [],
+      "minions" : [],
+      "creator" : creator,
+      "boss" : creator,
+      "testCommunity" : test
+
+  }, function(err, community) {
+    communityId.push(community._id);
+
+    User.findById(creator).exec(function(err, user) {
+      user.community = community._id;
+
+      user.save();
+
+    });
+  });
+}
