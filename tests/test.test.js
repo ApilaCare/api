a57ee22797a7b20c363b898633db3c894c2fe592
nenad <nesa993@gmail.com>
2016-08-05 13:45:04 +0200

    seting up test mocha and chai

---
+++ tests/test.test.js
@@ -0,0 +1,9 @@
+var assert = require('assert');
+
+describe('Nenad', function() {
+  describe('#Nenad', function() {
+    it('Test if Nenad is cool', function() {
+      assert.equal("Nenad is cool", "Nenad is cool");
+    });
+  });
+});
