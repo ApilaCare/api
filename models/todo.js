bb6200c9efc07ab7af4876ccddec79d9c1b06fc4
KyleRiggen <kyle.riggenbach@gmail.com>
2016-09-28 12:56:41 -0600

    initial attempt at the todo model

---
+++ models/todo.js
@@ -0,0 +1,50 @@
+var mongoose = require('mongoose');
+
+var todoItemSchema = new mongoose.Schema({
+
+  text: {type: String, required: true},
+  complete: {type: Boolean, required: true, default: false},
+  occurrence: {type: Number, required: true}, // scale of 1-13 | 1 = every hour,
+                                              //                 2 = twice a day,
+                                              //                 3 = every day,
+                                              //                 4 = every other day,
+                                              //                 5 = twice a week,
+                                              //                 6 = every week,
+                                              //                 7 = every two weeks,
+                                              //                 8 = twice a month,
+                                              //                 9 = every month,
+                                              //                 10 = every two months,
+                                              //                 11 = every quarter,
+                                              //                 12 = twice a year,
+                                              //                 13 = every year,
+
+  // automatic
+  createdOn: {type: Date, default: Date.now}
+  itemId: {type: Number, required: true},  // maybe have this create a ObjectId("xyz") for the subdocument
+});
+
+var counterSchema = new mongoose.Schema({
+  count: {type: Number, required: true, default: 0},
+  counterItemId: {type: Number, required: true}, // itemId from todoItemSchema
+});
+
+var todoSchema = new mongoose.Schema({
+
+  todoItem: [todoItemSchema],
+
+  // visual representation of the counters:
+  //
+  //    item                            becomes not completed;
+  //    posted         becomes          item posted
+  //    to do          overdue          to do again
+  //    |---------------|---------------|
+  //    |---------time interval---------|
+
+  // counters
+  completed: [counterSchema], // completed before certain amount of time
+  overDue: [counterSchema], // not completed
+  notCompleted: [counterSchema],
+
+});
+
+mongoose.model('To-Do', todoSchema);
