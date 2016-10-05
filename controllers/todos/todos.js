511907e97f1f6d6e17baa7c520af77b8eac004be
nenad <nesa993@gmail.com>
2016-10-05 18:25:31 +0200

    adding todo logic to the server just basic layout and a bit of todo model change

--- controllers/todos/todos.js
+++ controllers/todos/todos.js
@@ -4,6 +4,23 @@ var utils = require('../../services/utils');
 var ToDo = mongoose.model('ToDo');
 
 var _ = require('lodash');
+var moment = require('moment');
+
+var occurrence = {
+  "EVERY_HOUR" : 0,
+  "TWICE_DAY" : 1,
+  "EVERY_DAY" : 2,
+  "EVERY_OTHER_DAY" : 3,
+  "TWICE_WEEK" : 4,
+  "EVERY_WEEK" : 5,
+  "EVERY_TWO_WEEKS" : 6,
+  "TWICE_MONTH" : 7,
+  "EVERY_MONTH" : 8,
+  "EVERY_TWO_MONTHS" : 9,
+  "EVERY_QUATER" : 10,
+  "TWICE_YEAR" : 11,
+  "EVERY_YEAR" : 12,
+};
 
 // creates an empty todo object called when a user is registered
 module.exports.createEmptyToDo = function(callback) {
@@ -37,7 +54,15 @@ module.exports.listTasks = function(req, res) {
     if(err) {
       utils.sendJSONresponse(res, 500, err);
     } else {
-      utils.sendJSONresponse(res, 200, todo.tasks);
+
+      //before listing tasks check if any tasks are overdue/completed
+      updateTasks(todo, function(status, err) {
+        if(status){
+          utils.sendJSONresponse(res, 200, todo.tasks);
+        } else {
+            utils.sendJSONresponse(res, 500, err);
+        }
+      });
     }
   });
 
@@ -61,7 +86,8 @@ module.exports.addTask = function(req, res) {
       var newTask = {
         "text" : req.body.text,
         "occurrence" : req.body.occurrence,
-        "complete" : false
+        "complete" : false,
+        "cycleDate" : new Date()
       };
 
       todo.tasks.push(newTask);
@@ -103,7 +129,11 @@ module.exports.updateTask = function(req, res) {
 
         if(task.complete === true) {
           task.completed.push({"counter" : 0, updatedOn: new Date()});
+          task.current = false;
+          task.cycleDate = new Date();
         }
+
+        console.log(task);
 
         todo.tasks.set(index, task);
 
@@ -154,3 +184,56 @@ module.exports.deleteTask = function(req, res) {
   });
 
 };
+
+//if the task is current = true, show it in the current list of todoSchema
+// when we set overudue/notcomplete we set current = false
+
+// when a new day(cycle) rolls up we set current = true, so we need to remember
+// on what cycle (date) we are
+
+function updateTasks(todo, callback) {
+
+  var tasks = todo.tasks;
+  var currTime = moment();
+
+  _.forEach(tasks, function(task) {
+
+      inNewCycle(task);
+
+      // if(task.occurrence === occurrence.EVERY_DAY) {
+      //   //if it's past 12 pm it's overdue
+      //   if(currTime.hour() >= 12) {
+      //     console.log('overdue');
+      //     //if we are already have overdue for this cycle dont push it
+      //     task.overDue.push({"counter" : 0, updatedOn: new Date()});
+      //   }
+      // }
+
+  });
+
+  todo.tasks = tasks;
+
+  todo.save(function(err) {
+    if(err) {
+      callback(false, err);
+    } else {
+      callback(true, err);
+    }});
+
+}
+
+function inNewCycle(task) {
+
+  var currTime = moment();
+  var cycleDate = moment(task.cycleDate);
+
+  if(task.occurrence === occurrence.EVERY_DAY) {
+    //check if the current day and the cycle day is even to see if we are on the same cycle
+    if(!currTime.isSame(cycleDate, "day")){
+      console.log("Z date iz not d same");
+      task.current = true;
+      task.complete = false;
+      task.cycleDate = new Date();
+    }
+  }
+}
--- models/todo.js
+++ models/todo.js
@@ -4,31 +4,14 @@ var todoItemSchema = new mongoose.Schema({
 
   text: {type: String, required: true},
   complete: {type: Boolean, required: true, default: false},
-  occurrence: {type: Number, required: true}, // scale of 1-13 | 1 = every hour,
-                                              //                 2 = twice a day,
-                                              //                 3 = every day,
-                                              //                 4 = every other day,
-                                              //                 5 = twice a week,
-                                              //                 6 = every week,
-                                              //                 7 = every two weeks,
-                                              //                 8 = twice a month,
-                                              //                 9 = every month,
-                                              //                 10 = every two months,
-                                              //                 11 = every quarter,
-                                              //                 12 = twice a year,
-                                              //                 13 = every year,
+  current: {type: Boolean, default: true}, // if the task is currently ongoing
+  cycleDate : {type: Date, default: Date.now},
+  occurrence: {type: Number, required: true},
 
   // automatic
   completeOn: {type: Date},
   createdOn: {type: Date, default: Date.now},
 
-  // visual representation of the counters:
-  //
-  //    item                            becomes "not completed";
-  //    posted         becomes          item posted
-  //    to do          "overdue"        to do again
-  //      |---------------|---------------|
-  //     |---------time interval---------|
   completed: [counterSchema], // completed before certain amount of time
   overDue: [counterSchema], // not completed
   notCompleted: [counterSchema],
@@ -44,5 +27,27 @@ var todoSchema = new mongoose.Schema({
   tasks: [todoItemSchema]
 
 });
+
+// scale of 1-13 | 1 = every hour,
+//                 2 = twice a day,
+//                 3 = every day,
+//                 4 = every other day,
+//                 5 = twice a week,
+//                 6 = every week,
+//                 7 = every two weeks,
+//                 8 = twice a month,
+//                 9 = every month,
+//                 10 = every two months,
+//                 11 = every quarter,
+//                 12 = twice a year,
+//                 13 = every year,
+
+// visual representation of the counters:
+//
+//    item                            becomes "not completed";
+//    posted         becomes          item posted
+//    to do          "overdue"        to do again
+//      |---------------|---------------|
+//     |---------time interval---------|
 
 mongoose.model('ToDo', todoSchema);
