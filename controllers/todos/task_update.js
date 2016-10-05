98a031cc3af7db0e21ffdeb4e840ec2b5ed2ecd8
nenad <nesa993@gmail.com>
2016-10-05 20:44:16 +0300

    puting task logic code in a seperate file and work a bit more on occurence logic

---
+++ controllers/todos/task_update.js
@@ -0,0 +1,138 @@
+var _ = require('lodash');
+var moment = require('moment');
+
+var START_WORK_DAY = 8;
+var END_WORK_DAY = 16;
+
+var occurrence = {
+  "EVERY_HOUR" : 0,
+  "TWICE_DAY" : 1,
+  "EVERY_DAY" : 2,
+  "EVERY_OTHER_DAY" : 3,
+  "TWICE_WEEK" : 4,
+  "EVERY_WEEK" : 5,
+  "EVERY_TWO_WEEKS" : 6,
+  "TWICE_MONTH" : 7,
+  "EVERY_MONTH" : 8,
+  "EVERY_TWO_MONTHS" : 9,
+  "EVERY_QUATER" : 10,
+  "TWICE_YEAR" : 11,
+  "EVERY_YEAR" : 12,
+};
+
+
+//if the task is current = true, show it in the current list of todoSchema
+// when we set overudue/notcomplete we set current = false
+
+// when a new day(cycle) rolls up we set current = true, so we need to remember
+// on what cycle (date) we are
+
+module.exports.updateTasks = function(todo, callback) {
+
+  var tasks = todo.tasks;
+  var currTime = moment();
+
+  _.forEach(tasks, function(task) {
+
+      inNewCycle(task);
+
+      // if(task.occurrence === occurrence.EVERY_DAY) {
+      //   //if it's past 12 pm it's overdue
+      //   if(currTime.hour() >= 12) {
+      //     console.log('overdue');
+      //     //if we are already have overdue for this cycle dont push it
+      //     task.overDue.push({"counter" : 0, updatedOn: new Date()});
+      //   }
+      // }
+
+  });
+
+  todo.tasks = tasks;
+
+  todo.save(function(err) {
+    if(err) {
+      callback(false, err);
+    } else {
+      callback(true, err);
+    }});
+
+};
+
+//TODO: Monday - Friday??
+
+function inNewCycle(task) {
+
+  var currTime = moment();
+  var cycleDate = moment(task.cycleDate);
+
+  switch(task.occurrence) {
+
+    // Every Hour - task should be active between 8 - 16
+    case occurrence.EVERY_HOUR:
+        console.log(cycleDate.hour() + "  " + currTime.hour());
+        if(currTime.hour() >= START_WORK_DAY &&
+                currTime.hour() <= END_WORK_DAY && currTime.hour() !== cycleDate.hour()){
+
+          resetTaskCycle(task);
+        }
+      break;
+
+    // midnight and moon
+    case occurrence.TWICE_DAY:
+
+      break;
+
+    case occurrence.EVERY_DAY:
+        if(!currTime.isSame(cycleDate, "day")){
+          resetTaskCycle(task);
+        }
+      break;
+
+    case occurrence.EVERY_OTHER_DAY:
+
+      break;
+
+    case occurrence.TWICE_WEEK:
+
+      break;
+
+    case occurrence.EVERY_WEEK:
+
+      break;
+
+    case occurrence.EVERY_TWO_WEEKS:
+
+      break;
+
+    case occurrence.TWICE_MONTH:
+
+      break;
+
+    case occurrence.EVERY_MONTH:
+
+      break;
+
+    case occurrence.EVERY_TWO_MONTHS:
+
+      break;
+
+    case occurrence.EVERY_QUATER:
+
+      break;
+
+    case occurrence.TWICE_YEAR:
+
+      break;
+
+    case occurrence.EVERY_YEAR:
+
+      break;
+
+  }
+}
+
+function resetTaskCycle(task) {
+  task.current = true;
+  task.complete = false;
+  task.cycleDate = new Date();
+}
--- controllers/todos/todos.js
+++ controllers/todos/todos.js
@@ -2,25 +2,10 @@ var mongoose = require('mongoose');
 var utils = require('../../services/utils');
 
 var ToDo = mongoose.model('ToDo');
+var TaskService = require('./task_update');
 
 var _ = require('lodash');
 var moment = require('moment');
-
-var occurrence = {
-  "EVERY_HOUR" : 0,
-  "TWICE_DAY" : 1,
-  "EVERY_DAY" : 2,
-  "EVERY_OTHER_DAY" : 3,
-  "TWICE_WEEK" : 4,
-  "EVERY_WEEK" : 5,
-  "EVERY_TWO_WEEKS" : 6,
-  "TWICE_MONTH" : 7,
-  "EVERY_MONTH" : 8,
-  "EVERY_TWO_MONTHS" : 9,
-  "EVERY_QUATER" : 10,
-  "TWICE_YEAR" : 11,
-  "EVERY_YEAR" : 12,
-};
 
 // creates an empty todo object called when a user is registered
 module.exports.createEmptyToDo = function(callback) {
@@ -56,7 +41,7 @@ module.exports.listTasks = function(req, res) {
     } else {
 
       //before listing tasks check if any tasks are overdue/completed
-      updateTasks(todo, function(status, err) {
+      TaskService.updateTasks(todo, function(status, err) {
         if(status){
           utils.sendJSONresponse(res, 200, todo.tasks);
         } else {
@@ -184,56 +169,3 @@ module.exports.deleteTask = function(req, res) {
   });
 
 };
-
-//if the task is current = true, show it in the current list of todoSchema
-// when we set overudue/notcomplete we set current = false
-
-// when a new day(cycle) rolls up we set current = true, so we need to remember
-// on what cycle (date) we are
-
-function updateTasks(todo, callback) {
-
-  var tasks = todo.tasks;
-  var currTime = moment();
-
-  _.forEach(tasks, function(task) {
-
-      inNewCycle(task);
-
-      // if(task.occurrence === occurrence.EVERY_DAY) {
-      //   //if it's past 12 pm it's overdue
-      //   if(currTime.hour() >= 12) {
-      //     console.log('overdue');
-      //     //if we are already have overdue for this cycle dont push it
-      //     task.overDue.push({"counter" : 0, updatedOn: new Date()});
-      //   }
-      // }
-
-  });
-
-  todo.tasks = tasks;
-
-  todo.save(function(err) {
-    if(err) {
-      callback(false, err);
-    } else {
-      callback(true, err);
-    }});
-
-}
-
-function inNewCycle(task) {
-
-  var currTime = moment();
-  var cycleDate = moment(task.cycleDate);
-
-  if(task.occurrence === occurrence.EVERY_DAY) {
-    //check if the current day and the cycle day is even to see if we are on the same cycle
-    if(!currTime.isSame(cycleDate, "day")){
-      console.log("Z date iz not d same");
-      task.current = true;
-      task.complete = false;
-      task.cycleDate = new Date();
-    }
-  }
-}
