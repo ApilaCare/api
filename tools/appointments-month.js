5e6ebbdd0c16d3449754a4b4827f5d5134a7c9bb
nenad <nesa993@gmail.com>
2017-01-11 13:49:57 +0100

    adding a script to populate current month field

---
+++ tools/appointments-month.js
@@ -0,0 +1,26 @@
+require('../models/db');
+
+const mongoose = require('mongoose');
+const Appoint = mongoose.model('Appointment');
+
+const moment = require('moment');
+
+const asyncLib = require('async');
+
+(() => {
+  Appoint.find({}).exec(function(err, appointments) {
+    if(!err) {
+
+      asyncLib.each(appointments, function(appointment) {
+        appointment.currMonth = moment(appointment.appointmentDate).format("YYYY M");
+
+       appointment.save();
+      });
+
+      console.log("appointments updated");
+
+    } else {
+      console.log("Error while finding appointments");
+    }
+  });
+})();
