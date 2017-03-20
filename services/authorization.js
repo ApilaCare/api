fd9de53f4a55d21e4cfe4d2f94be1f5f8b083546
nenad <nesa993@gmail.com>
2017-03-20 17:42:15 +0100

    extending the authorization middle ware and adding checks for boss role

--- services/authorization.js
+++ services/authorization.js
@@ -1,9 +1,10 @@
 const utils = require('./utils');
-
+const mongoose = require('mongoose');
+const Community = mongoose.model('Community');
 
 module.exports = (options) => {
 
-    return  (req, res, next) => {
+    return  async (req, res, next) => {
         
         const userInfo = req.payload;
 
@@ -13,6 +14,12 @@ module.exports = (options) => {
 
                 const requestedCommunity = req.params.communityid || req.params.communityId;
 
+                //no community id was found
+                if(!requestedCommunity) {
+                    next();
+                    return;
+                }
+
                 if(usersCommunity !== requestedCommunity) {
                     console.log('Requesting something that isnt from your community');
                     utils.sendJSONresponse(res, 404, {err: 'Requesting something that isnt from your community'});
@@ -21,7 +28,15 @@ module.exports = (options) => {
             }
 
             if(options.boss) {
-                
+
+                const userIsBoss = await isBoss(userInfo.community, userInfo._id);
+
+                console.log(userIsBoss);
+
+                if(!userIsBoss) {
+                    utils.sendJSONresponse(res, 404, {err: "User is not a boss"});
+                    return;
+                }
             }
         } else {
             console.log('Authorization failed must be called after the auth middleware');
@@ -30,3 +45,15 @@ module.exports = (options) => {
         next();
     }
 };
+
+async function isBoss(communityid, boss) {
+
+    const community = await Community.findById(communityid);
+
+    if(String(community.boss) === String(boss)) {
+        return true;
+    } else {
+        return false;
+    }
+
+}
