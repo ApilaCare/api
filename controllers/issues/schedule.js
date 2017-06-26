245aa931cfc58c6ab61ad50cc982387cb7cdd6bd
nenad <nesa993@gmail.com>
2017-06-26 14:17:58 +0200

    working on schedule update each day on the api for calculating activity score

--- controllers/issues/schedule.js
+++ controllers/issues/schedule.js
@@ -1,19 +1,20 @@
-var mongoose = require('mongoose');
-var schedule = require('node-schedule');
-var Iss = mongoose.model('Issue');
+const mongoose = require('mongoose');
+const schedule = require('node-schedule');
+const Iss = mongoose.model('Issue');
+const moment = require('moment');
 
 // constants time in minutes
-var DAY = 24 * 60;
-var THREE_DAYS = 72 * 60;
-var THREE_WEEKS = 504 * 60;
-var THREE_MONTHS = 2160 * 60;
+const DAY = 24 * 60;
+const THREE_DAYS = 72 * 60;
+const THREE_WEEKS = 504 * 60;
+const THREE_MONTHS = 2160 * 60;
 
-var rule = new schedule.RecurrenceRule();
+let rule = new schedule.RecurrenceRule();
 rule.hour = 1;
 
 //checks each hour if any of the issues are 'experied'
 //need to change status after some time has passed
-schedule.scheduleJob(rule, function() {
+schedule.scheduleJob(rule, () => {
 
   // get all shelved issues
   Iss.find({
@@ -50,6 +51,42 @@ schedule.scheduleJob(rule, function() {
 
 });
 
+
+const everyDayRule = new schedule.RecurrenceRule();
+
+//just for testing
+everyDayRule.minute = 1;
+
+schedule.scheduleJob(" */5 * * * *", async () => {
+
+  let users = {};
+
+  const allIssues = await Iss.find({}).exec();
+
+  allIssues.forEach((issue) => {
+    const activeIssue = isIssueActive(issue);
+
+    trackUserActivity(users, issue, activeIssue);
+  });
+
+  console.log(users);
+
+ 
+});
+
+function trackUserActivity(users, issue, activeIssue) {
+  if(!users[issue.responsibleParty]) {
+    users[issue.responsibleParty] = {inactiveIssues: 0, activeIssues: 0};
+  }
+
+  if(activeIssue) {
+    users[issue.responsibleParty].activeIssues++;
+  } else {
+    users[issue.responsibleParty].inactiveIssues++;
+  }
+
+}
+
 //given an issues Id it changes its status to Open
 function changeIssueStatus(id) {
   Iss.findById(id).exec(function(err, issue) {
@@ -60,4 +97,46 @@ function changeIssueStatus(id) {
       console.log("issue not found");
     }
   });
+}
+
+function isIssueActive(issue) {
+
+    let lastDate = "";
+
+    if(issue.updateInfo.length > 0) {
+      lastDate = issue.updateInfo[issue.updateInfo.length - 1].updateDate;
+    } else {
+      lastDate = issue.submitDate;
+    }
+
+    switch(issue.resolutionTimeframe) {
+      case "Hours":
+        return checkDateOffset(issue, lastDate, 'hours');
+      break;
+
+      case "Days":
+        return checkDateOffset(issue, lastDate, 'days');
+      break;
+
+      case "Weeks":
+        return checkDateOffset(issue, lastDate, 'weeks');
+      break;
+
+      case "Months":
+        return checkDateOffset(issue, lastDate, 'months');
+      break;
+
+    }
+    
+}
+
+function checkDateOffset(issue, lastDate, timeUnit) {
+  const offsetDate = moment(lastDate).add(1, timeUnit);
+
+  if(moment().isAfter(offsetDate)) {
+    return true;
+  }
+
+  return false;
+
 }
